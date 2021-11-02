a21db2978934c20f6061a29cd60e54a79c8f886e
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-11-02 14:29:02 -0700

    Added Domain classes
    Added Domain classes: SubCategory, Category and Event

---
+++ EventManagement/EventAPI/Domain/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventAPI.Domain
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string EventImageUrl { get; set; }
+        public int MaxOccupancy { get; set; }
+        public int MaxTicketsPerUser { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool isPaidEvent { get; set; }
+        public bool isOnlineEvent { get; set; }
+        public bool isCancelled { get; set; }
+        public string EventLinkUrl { get; set; }
+        public virtual Category Category { get; set; }
+        
+        //Need to uncomment these FK fields once corresponding classes are ready.
+        //public virtual Location Location { get; set; }
+        
+        //public virtual User User { get; set; }
+    }
+}
