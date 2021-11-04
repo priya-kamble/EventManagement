d142d4acb6a43c05922ba7d2b0816f92ce940727
priya-kamble <priya90kamble@gmail.com>
2021-11-04 00:31:14 -0700

    1.Added EventCatalog Context for DBContext. 2.Added EventCatalogSeed class. 3.Removed SubCategory from Category class. 4.Added SubCategory and UserEmailId property to Event class. 5.Renamed UserName to UserEmailId and Contacts to Contact Number in Organization class. 6.Added navigation to category from SubCategory. 7.Included generated Migration folder to EventAPI project. 8.Added lines of code to call the Seed method in Program.cs.

---
+++ EventManagement/EventAPI/Data/EventCatalogSeed.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventAPI.Data
+{
+    public static class EventCatalogSeed
+    {
+        public static void Seed(EventCatalogContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
