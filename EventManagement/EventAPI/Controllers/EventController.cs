853c6ee137c5eb6fc68f299f8193ab4a5e451970
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-11-07 10:03:53 -0800

    APIs
    - Event API with pagination
    - API with Date sort
    - PaginatedEventViewModel

--- EventManagement/EventAPI/Controllers/EventController.cs
+++ EventManagement/EventAPI/Controllers/EventController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EventAPI.Data;
+using EventAPI.Domain;
+using EventAPI.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +15,66 @@ namespace EventAPI.Controllers
     [ApiController]
     public class EventController : ControllerBase
     {
+        public readonly EventCatalogContext _context;
+        public EventController(EventCatalogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("[Action]")]
+        public async Task<IActionResult> Events([FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 6)
+        {
+            var eventsCount = _context.EventCatalog.LongCountAsync();
+            var events = await _context.EventCatalog
+                                    .OrderBy(e => e.Id)
+                                    .Skip(pageSize * pageIndex)
+                                    .Take(pageSize)
+                                    .ToListAsync();
+            var model = new PaginatedEventsViewModel
+            {
+                Data = events,
+                PageIndex = pageIndex,
+                PageSize = events.Count,
+                Count = eventsCount.Result
+            };
+
+            return Ok(model);
+        }
+
+        [HttpGet("[Action]")]
+        public async Task<IActionResult> EventDates(
+            [FromQuery] DateTime? startDate,
+            [FromQuery] DateTime? endDate,
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+
+            var query = (IQueryable<Event>)_context.EventCatalog;
+            if (startDate.HasValue)
+            {
+                query = query.Where(e => e.StartDate == startDate);
+            }
+
+            if (endDate.HasValue)
+            {
+                query = query.Where(e => e.EndDate == endDate);
+            }
+
+            var eventsCount = query.LongCountAsync();
+            var events = await query
+                                    .OrderBy(e => e.Id)
+                                    .Skip(pageSize * pageIndex)
+                                    .Take(pageSize)
+                                    .ToListAsync();
+            var model = new PaginatedEventsViewModel
+            {
+                Data = events,
+                PageIndex = pageIndex,
+                PageSize = events.Count,
+                Count = eventsCount.Result
+            };
+
+            return Ok(model);
+        }
     }
 }
---
+++ EventManagement/EventAPI/ViewModel/PaginatedEventsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EventAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventAPI.ViewModel
+{
+    public class PaginatedEventsViewModel
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; } // Size that is returned back
+        public long Count { get; set; }
+        public IEnumerable<Event> Data { get; set; }
+
+    }
+}
