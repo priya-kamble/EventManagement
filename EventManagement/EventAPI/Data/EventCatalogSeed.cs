bbf00bbf8ca03acdad0a7363296dd1224f73ff11
Filicity SH <f.elena.sh@gmail.com>
2021-11-09 13:52:11 -0800

    Saving data in  Categoey,Subcategory and Format  tables changed to indivitual row

--- EventManagement/EventAPI/Data/EventCatalogSeed.cs
+++ EventManagement/EventAPI/Data/EventCatalogSeed.cs
@@ -14,19 +14,19 @@ namespace EventAPI.Data
             context.Database.Migrate();
             if (!context.Categories.Any())
             {
-                context.Categories.AddRange(GetCategories());
-                context.SaveChanges();
+                SaveCategoryData(context);
+               
             }
             if (!context.SubCategories.Any())
             {
-                context.SubCategories.AddRange(GetSubCategories());
-                context.SaveChanges();
+
+                SaveSubCategoryData(context);
+                
             }
 
             if (!context.Formats.Any())
             {
-                context.Formats.AddRange(GetFormats());
-                context.SaveChanges();
+                SaveFormatsData( context);
             }
 
             if (!context.Users.Any())
@@ -53,9 +53,15 @@ namespace EventAPI.Data
                 context.SaveChanges();
             }
         }
-        private static IEnumerable<Category> GetCategories()
+
+    
+
+        private static void SaveCategoryData (EventCatalogContext context)
         {
-            return new List<Category>
+
+            List<Category> CategoryData;
+           
+            CategoryData = new List<Category>             
             {
                
                 new Category { CategoryName = "Business & Professional" },
@@ -66,13 +72,20 @@ namespace EventAPI.Data
                 new Category { CategoryName = "School Activities" },
                 new Category { CategoryName = "Sports & Fitness" },
                 new Category { CategoryName = "Travel & Outdoor" }
-            };
-        }
-        private static IEnumerable<SubCategory> GetSubCategories()
-        {
-            return new List<SubCategory>
+               };
+
+            foreach (var data in CategoryData)
             {
-                
+                context.Categories.Add(data);
+                context.SaveChanges();
+            }
+        }
+        private static void SaveSubCategoryData(EventCatalogContext context)
+        {
+            List<SubCategory> SubCategoryData;
+            SubCategoryData= new List<SubCategory>
+            {
+
                 new SubCategory { SubCategoryName = "Career", CategoryId = 1 },
 
                 new SubCategory { SubCategoryName = "Real Estate", CategoryId = 1 },
@@ -86,12 +99,20 @@ namespace EventAPI.Data
                 new SubCategory { SubCategoryName = "League", CategoryId = 7 },
                 new SubCategory { SubCategoryName = "Travel", CategoryId = 8 }
             };
-        }
-        private static IEnumerable<Format> GetFormats()
-        {
-            return new List<Format>
+            foreach (var data in SubCategoryData)
             {
-            
+                context.SubCategories.Add(data);
+                context.SaveChanges();
+            }
+
+        }
+        private static void SaveFormatsData(EventCatalogContext context)
+        {
+
+            List<Format> EventFormat;
+
+            EventFormat= new List<Format>
+            {           
                 new Format { FormatName = "Camp, Trip, or Retreat" },
                 new Format { FormatName = "Class, Training, or Workshop" },
                 new Format { FormatName = "Conference" },
@@ -104,6 +125,13 @@ namespace EventAPI.Data
                 new Format { FormatName = "Dinner or Gala" } ,
                 new Format { FormatName = "Camp, Trip, or Retreat" }
              };
+
+            foreach (var data in EventFormat)
+            {
+                context.Formats.Add(data);
+                context.SaveChanges();
+            }
+
         }
         private static IEnumerable<Organization> GetOrganizations()
         {
@@ -234,17 +262,17 @@ namespace EventAPI.Data
                 new Event { Title = "Harmony", Description = "A musical workshop", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9",
                     MaxOccupancy = 15, MaxTicketsPerUser = 2, StartDate = new DateTime(2022, 1, 1), EndDate = new DateTime(2022, 1, 1) , IsPaidEvent = false ,
                     IsOnlineEvent = false, IsCancelled = false, EventLinkUrl = "http://itneedstobereplaced/eventlink/1", LocationId = 4, Address = "8352 Marvon Dr.",
-                    SubCategoryId = 4, UserId = 3, FormatId = 11 },
+                    SubCategoryId = 4, UserId = 3, FormatId = 2 },
 
                 new Event { Title = "Cruise Away", Description = "Exploring the sea together", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/10",
                     MaxOccupancy = 150, MaxTicketsPerUser = 6, StartDate = new DateTime(2022, 1, 1), EndDate = new DateTime(2022, 3, 3) , IsPaidEvent = false ,
                     IsOnlineEvent = false, IsCancelled = false, EventLinkUrl = "http://itneedstobereplaced/eventlink/1", LocationId = 6, Address = "1000 1st Ave",
-                    SubCategoryId = 10, UserId = 3, FormatId = 11 },
+                    SubCategoryId = 11, UserId = 3, FormatId = 11 },
 
                 new Event { Title = "Park & Hike Event", Description = "Hike in the woods", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/11",
                     MaxOccupancy = 20, MaxTicketsPerUser = 4, StartDate = new DateTime(2022, 3, 3), EndDate = new DateTime(2022, 3, 3) , IsPaidEvent = false ,
                     IsOnlineEvent = false, IsCancelled = false, EventLinkUrl = "http://itneedstobereplaced/eventlink/1", LocationId = 1, Address = "8352 Marvon Dr.",
-                    SubCategoryId = 8, UserId = 3, FormatId = 6 },
+                    SubCategoryId = 9, UserId = 3, FormatId = 6 },
 
                 new Event { Title = "Family.Life.Education", Description = "Learn-Grow-Fulfill your Potential", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12",
                     MaxOccupancy = 6, MaxTicketsPerUser = 4, StartDate = new DateTime(2022, 3, 3), EndDate = new DateTime(2022, 3, 3) , IsPaidEvent = false ,
@@ -259,7 +287,7 @@ namespace EventAPI.Data
                 new Event { Title = "After School Enrichment Programs", Description = "S.T.E.M", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14",
                     MaxOccupancy = 15, MaxTicketsPerUser = 1, StartDate = new DateTime(2022, 1, 1), EndDate = new DateTime(2022, 2, 2) , IsPaidEvent = false ,
                     IsOnlineEvent = false, IsCancelled = true, EventLinkUrl = "http://itneedstobereplaced/eventlink/1", LocationId = 8, Address = "219 Madison Ave",
-                    SubCategoryId = 8, UserId = 3, FormatId = 3 },
+                    SubCategoryId = 8, UserId = 3, FormatId = 2 },
 
                 new Event { Title = "Seattle Job Fair", Description = "Seattle Career Fair", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15",
                     MaxOccupancy = 30, MaxTicketsPerUser = 1, StartDate = new DateTime(2022, 2, 2), EndDate = new DateTime(2022, 2, 2) , IsPaidEvent = false ,
@@ -269,7 +297,7 @@ namespace EventAPI.Data
                 new Event { Title = "Meet us at the sky", Description = "Hot Air Balloon adventure ride", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/16",
                     MaxOccupancy = 25, MaxTicketsPerUser = 4, StartDate = new DateTime(2022, 2, 2), EndDate = new DateTime(2022, 2, 2) , IsPaidEvent = false ,
                     IsOnlineEvent = false, IsCancelled = false, EventLinkUrl = "http://itneedstobereplaced/eventlink/1", LocationId = 3, Address = "219 Madison Ave",
-                    SubCategoryId = 10, UserId = 3, FormatId = 11 },
+                    SubCategoryId = 11, UserId = 3, FormatId = 11 },
 
                 new Event { Title = "Vector Stock", Description = "A complete wellness guide", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/17",
                     MaxOccupancy = 10, MaxTicketsPerUser = 1, StartDate = new DateTime(2022, 2, 2), EndDate = new DateTime(2022, 2, 2) , IsPaidEvent = false ,
@@ -279,7 +307,7 @@ namespace EventAPI.Data
                 new Event { Title = "Pro Club", Description = "A complete sports league", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/18",
                     MaxOccupancy = 20, MaxTicketsPerUser = 1, StartDate = new DateTime(2022, 3, 3), EndDate = new DateTime(2022, 4, 4) , IsPaidEvent = false ,
                     IsOnlineEvent = false, IsCancelled = true, EventLinkUrl = "http://itneedstobereplaced/eventlink/1", LocationId = 1, Address = "219 Madison Ave",
-                    SubCategoryId = 9, UserId = 3, FormatId = 5 },
+                    SubCategoryId = 10, UserId = 3, FormatId = 5 },
 
                 new Event { Title = "Mind, Heart & Soul", Description = "A complete You!!", EventImageUrl = "http://externalcatalogbaseurltobereplaced/api/pic/19",
                     MaxOccupancy = 15, MaxTicketsPerUser = 1, StartDate = new DateTime(2022, 1, 1), EndDate = new DateTime(2022, 2, 2) , IsPaidEvent = false ,
