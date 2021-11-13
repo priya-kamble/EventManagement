508aa8d9f8e29aeafa6aca7d041abc8b0d941225
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-11-12 21:02:26 -0800

    ViewModels for WebMvc
    ViewModels for WebMvc

---
+++ EventManagement/WebMvc/ViewModels/EventIndexViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models;
+
+namespace WebMvc.ViewModels
+{
+    public class EventIndexViewModel
+    {
+        public IEnumerable<SelectListItem> Location { get; set; }
+        public IEnumerable<SelectListItem> Format { get; set; }
+        public IEnumerable<SelectListItem> Category { get; set; }
+        public IEnumerable<SelectListItem> Date { get; set; }
+        public IEnumerable<SelectListItem> Price { get; set; }
+        public Boolean Online { get; set; }
+        public IEnumerable<Event> Events { get; set; }
+        public PaginationInfo PaginationInfo { get; set; }
+
+        public int? LocationFilterApplied { get; set; }
+        public int? FormatFilterApplied { get; set; }
+        public int? CategoryFilterApplied { get; set; }
+        public int? DateFilterApplied { get; set; }
+        public int? PriceFilterApplied { get; set; }
+        public int? OnlineFilterApplied { get; set; }
+    }
+}
