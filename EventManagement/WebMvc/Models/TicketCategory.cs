1efbfb4bcf1c9468ab3277c0bdef15f11450fa27
Priya Kamble <priya90kamble@gmail.com>
2021-12-03 12:33:59 -0800

    Model for Ticket API in WebMVC

---
+++ EventManagement/WebMvc/Models/TicketCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace WebMvc.Models
+{
+    public class TicketCategory
+    {
+        public int TicketCategoryId { get; set; }
+        public string TicketCategoryName { get; set; }
+    }
+}
