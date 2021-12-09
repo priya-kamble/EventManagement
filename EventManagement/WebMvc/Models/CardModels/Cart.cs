9525a0f57bf7c4fc1186752c1319b9e30fc70711
Filicity SH <f.elena.sh@gmail.com>
2021-12-08 16:37:28 -0800

    Cart and CartItem in MVCpart has been adde

---
+++ EventManagement/WebMvc/Models/CardModels/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Models
+{
+    public class Cart
+    {
+        public string UserId { get; set; }
+        public List<CartItem> CardTickets { get; set; } = new List<CartItem>();
+   
+        public decimal Total()
+        {
+            return Math.Round(CardTickets.Sum(x => x.TicketPrice * x.Quantity), 2);
+        }
+
+    }
+}
