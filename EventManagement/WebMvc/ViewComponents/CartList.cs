79163ccc6fcc41cdeec015fe27db80b2f06d0c9f
Priya Kamble <priya90kamble@gmail.com>
2021-12-10 14:52:32 -0800

    Added Cart.cs and CartList.cs in ViewComponent folder.

---
+++ EventManagement/WebMvc/ViewComponents/CartList.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Polly.CircuitBreaker;
+using System.Threading.Tasks;
+using WebMvc.Models;
+using WebMvc.Services;
+using WebMvc.ViewModels;
+
+
+namespace WebMvc.ViewComponents
+{
+    public class CartList : ViewComponent
+    {
+        private readonly ICartService _cartSvc;
+
+        public CartList(ICartService cartSvc) => _cartSvc = cartSvc;
+        public async Task<IViewComponentResult> InvokeAsync(ApplicationUser user)
+        {
+            var vm = new Models.CartModels.Cart();
+            try
+            {
+                vm = await _cartSvc.GetCart(user);
+
+
+                return View(vm);
+            }
+            catch (BrokenCircuitException)
+            {
+                ViewBag.IsBasketInoperative = true;
+                TempData["BasketInoperativeMsg"] = "Basket Service is inoperative, please try later on. (Business Msg Due to Circuit-Breaker)";
+            }
+            return View(vm);
+        }
+    }
+}
