403ef20e9f01b9207db30fa59f3c988b7be802ff
AndrewPBartson <andrewtestarea@gmail.com>
2021-12-15 14:25:08 -0800

    Add Data folder, OrdersDbContext.cs, MigrateDatabase.cs

---
+++ EventManagement/OrderAPI/Data/MigrateDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Data;
+using OrderAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderAPI.Data
+{
+    public static class MigrateDatabase
+    {
+        public static void EnsureCreated(OrdersContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
