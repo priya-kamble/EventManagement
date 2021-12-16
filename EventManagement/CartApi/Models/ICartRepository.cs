33012eb41f7d984b7cdce2b023a8ab3e230844b8
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-12-15 23:20:04 -0800

    Updated namespaces for CartAPI
    Updated namespaces for CartAPI

--- EventManagement/CartApi/Models/ICartRepository.cs
+++ EventManagement/CartApi/Models/ICartRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CartApi.Models
+namespace CartAPI.Models
 {
     public interface ICartRepository
     {
