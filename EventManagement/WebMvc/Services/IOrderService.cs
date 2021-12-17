1c427951b097a9e26ac2f5db276ae4b65533a745
Priya Kamble <priya90kamble@gmail.com>
2021-12-16 17:45:29 -0800

    Added IOrderService and OrderService to the WebMvc.

---
+++ EventManagement/WebMvc/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebMvc.Models.OrderModels;
+
+namespace WebMvc.Services
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetOrders();
+        Task<Order> GetOrder(string orderId);
+        Task<int> CreateOrder(Order order);
+    }
+}
